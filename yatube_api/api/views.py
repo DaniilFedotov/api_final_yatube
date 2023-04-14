e48cd7ebf5990f735afbf3f419c02a4b2d04d352
Daniil Fedotov <pustovdanil@bk.ru>
2023-04-14 21:21:48 +0500

    first commit

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,52 @@
-# TODO:  Напишите свой вариант
+from rest_framework import viewsets, permissions
+from django.shortcuts import get_object_or_404
+
+from posts.models import Post, Group, Comment, Follow, User
+from .serializers import PostSerializer, GroupSerializer, CommentSerializer, FollowSerializer
+from .permissions import AuthorOrReadOnly
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (AuthorOrReadOnly,)
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = (AuthorOrReadOnly,)
+
+    def get_queryset(self):
+        post = get_object_or_404(Post, id=self.kwargs.get('post_id'))
+        comments = post.comments.all()
+        return comments
+
+    def perform_create(self, serializer):
+        serializer.save(
+            author=self.request.user,
+            post=get_object_or_404(Post, id=self.kwargs.get('post_id'))
+        )
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = (AuthorOrReadOnly,)
+
+
+class FollowViewSet(viewsets.ModelViewSet):
+    serializer_class = FollowSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_queryset(self):
+        user = self.request.user
+        following = user.following.all()
+        return following
+
+    def perform_create(self, serializer):
+        serializer.save(
+            user=self.request.user
+        )
