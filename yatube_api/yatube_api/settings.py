f6343c270035fd5cc2de719e36383760d6d733c8
Daniil Fedotov <pustovdanil@bk.ru>
2023-04-18 00:38:36 +0500

    add blank lines for installed apps

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -17,9 +17,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'rest_framework',
     'django_filters',
     'djoser',
+
     'api.apps.ApiConfig',
     'posts.apps.PostsConfig',
 ]
